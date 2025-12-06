2b78d76c978f75ec305350a8669fe8a5ac31210b
Maximillian Whelan <maximillian.whelan@protonmail.com>
2025-12-06 11:59:27 +0000

    tidy up of day2 code

--- day2/day2.go
+++ day2/day2.go
@@ -6,6 +6,10 @@ import (
 	"strings"
 )
 
+// Finds the duplicates in the range between start and end
+// A duplicate is a repeating character integer, some examples would be
+// 11, 22, 33, 44, 123123, 12341234
+// this will return an integer, of the sum of these duplicates
 func SolveP1(s []string) int {
 	res := 0
 	for _, vr := range s {
@@ -15,19 +19,6 @@ func SolveP1(s []string) int {
 	return res
 }
 
-func SolveP2(s []string) int {
-	res := 0
-	for _, vr := range s {
-		split := strings.Split(vr, "-")
-		res += FindDupsInRangeP2(split[0], split[1])
-	}
-	return res
-}
-
-// Finds the duplicates in the range between start and end
-// A duplicate is a repeating character integer, some examples would be
-// 11, 22, 33, 44, 123123, 12341234
-// this will return an integer, of the sum of these duplicates
 func FindDupsInRange(start, end string) int {
 	res := 0
 	startI, err_start := strconv.Atoi(start)
@@ -44,6 +35,30 @@ func FindDupsInRange(start, end string) int {
 		if IsDuplicate(str) {
 			res += i
 		}
+	}
+	return res
+}
+
+func IsDuplicate(s string) bool {
+	// find the midpoint of the string using len / 2
+	middle := len(s) / 2
+
+	odd := mod(len(s), 2) != 0
+
+	// if odd just return false
+	if odd {
+		return false
+	} else {
+		// if even check [0:middle] [middle+1, end]
+		return s[0:middle] == s[middle:]
+	}
+}
+
+func SolveP2(s []string) int {
+	res := 0
+	for _, vr := range s {
+		split := strings.Split(vr, "-")
+		res += FindDupsInRangeP2(split[0], split[1])
 	}
 	return res
 }
@@ -68,23 +83,8 @@ func FindDupsInRangeP2(start, end string) int {
 	return res
 }
 
-func IsDuplicate(s string) bool {
-	// find the midpoint of the string using len / 2
-	middle := len(s) / 2
-
-	odd := mod(len(s), 2) != 0
-
-	// if odd just return false
-	if odd {
-		return false
-	} else {
-		// if even check [0:middle] [middle+1, end]
-		return s[0:middle] == s[middle:]
-	}
-}
-
 func IsDuplicateP2(s string) bool {
-	for i:=1; i<6; i++{ 
+	for i := 1; i < 6; i++ {
 		if groupsOfK(s, i) {
 			return true
 		}
@@ -96,7 +96,7 @@ func groupsOfK(s string, k int) bool {
 	if k == 1 && len(s) < 2 {
 		return false
 	}
-	if k!= 1 && len(s) < k*2 || mod(len(s), k) != 0 {
+	if k != 1 && len(s) < k*2 || mod(len(s), k) != 0 {
 		return false
 	}
 
@@ -107,18 +107,6 @@ func groupsOfK(s string, k int) bool {
 			return false
 		} else {
 			prevGroup = s[i-(k-1) : i+1]
-		}
-	}
-	return true
-}
-
-func groupsOf1(s string) bool {
-	if len(s) < 2 {
-		return false
-	}
-	for i := range len(s) {
-		if s[i] != s[0] {
-			return false
 		}
 	}
 	return true
